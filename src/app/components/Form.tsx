748b4fba51469e8f19730b5e61a9f2bd1729d8f9
arcav <armandojcastro84@gmail.com>
2024-09-26 13:12:37 -0400

    chore: format code using Prettier

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -6,24 +6,38 @@ interface FormProps {
     setOriginalUrl: React.Dispatch<React.SetStateAction<string>>;
     setError: React.Dispatch<React.SetStateAction<string>>;
     error: string;
-    
 }
 
-const Form: React.FC<FormProps> = ({ handleSubmit, originalUrl, setOriginalUrl, error }) => {
+const Form: React.FC<FormProps> = ({
+    handleSubmit,
+    originalUrl,
+    setOriginalUrl,
+    error,
+}) => {
     return (
-        <form onSubmit={handleSubmit} className='flex flex-col place-items-center space-y-4 p-2'>
+        <form
+            onSubmit={handleSubmit}
+            className='flex flex-col place-items-center space-y-4 p-2'>
             <input
-                type="text"
+                type='text'
                 value={originalUrl}
                 onChange={(e) => setOriginalUrl(e.target.value)}
-                onInvalid={(e) => (e.target as HTMLInputElement).setCustomValidity('Por favor, ingrese una URL válida.')}
-                onInput={(e) => (e.target as HTMLInputElement).setCustomValidity('')}
-                placeholder="Ingrese la URL original"
+                onInvalid={(e) =>
+                    (e.target as HTMLInputElement).setCustomValidity(
+                        'Por favor, ingrese una URL válida.',
+                    )
+                }
+                onInput={(e) =>
+                    (e.target as HTMLInputElement).setCustomValidity('')
+                }
+                placeholder='Ingrese la URL original'
                 className='w-full p-2 border rounded lg:w-1/2 flex justify-center'
-                required 
+                required
             />
             {error && <p className='text-black'>{error}</p>}
-            <button type="submit" className='p-2 bg-blue-500 text-white rounded'>
+            <button
+                type='submit'
+                className='p-2 bg-blue-500 text-white rounded'>
                 Acortar URL
             </button>
         </form>
